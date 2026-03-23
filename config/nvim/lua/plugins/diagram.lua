b8965f68c6461505452a3c74e68b606d1acba156
Tatsuya Sakurai <s.tatsuya0123@gmail.com>
2026-03-24 01:48:39 +0900

    chore: エラーに対応

--- config/nvim/lua/plugins/diagram.lua
+++ config/nvim/lua/plugins/diagram.lua
@@ -8,13 +8,15 @@ return {
     {
         "3rd/diagram.nvim",
         dependencies = { "3rd/image.nvim" },
-        opts = {
-            integrations = {
-                require("diagram.integrations.markdown"),
-            },
-            renderer_options = {
-                mermaid = { background = "transparent" },
-            },
-        },
+        opts = function()
+            return {
+                integrations = {
+                    require("diagram.integrations.markdown"),
+                },
+                renderer_options = {
+                    mermaid = { background = "transparent" },
+                },
+            }
+        end,
     },
 }
