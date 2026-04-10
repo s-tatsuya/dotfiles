274bb3cde2f565787195b24d0e0dc6b001a500e8
Tatsuya Sakurai <s.tatsuya0123@gmail.com>
2026-04-11 01:31:37 +0900

    chore: 設定をアップデート vim pluginのエラーは解決できず

--- config/nvim/lua/plugins/diagram.lua
+++ config/nvim/lua/plugins/diagram.lua
@@ -1,22 +1,22 @@
-return {
-    {
-        "3rd/image.nvim",
-        opts = {
-            backend = "kitty",
-        },
-    },
-    {
-        "3rd/diagram.nvim",
-        dependencies = { "3rd/image.nvim" },
-        opts = function()
-            return {
-                integrations = {
-                    require("diagram.integrations.markdown"),
-                },
-                renderer_options = {
-                    mermaid = { background = "transparent" },
-                },
-            }
-        end,
-    },
-}
+-- return {
+--     {
+--         "3rd/image.nvim",
+--         opts = {
+--             backend = "kitty",
+--         },
+--     },
+--     {
+--         "3rd/diagram.nvim",
+--         dependencies = { "3rd/image.nvim" },
+--         opts = function()
+--             return {
+--                 integrations = {
+--                     require("diagram.integrations.markdown"),
+--                 },
+--                 renderer_options = {
+--                     mermaid = { background = "transparent" },
+--                 },
+--             }
+--         end,
+--     },
+-- }
