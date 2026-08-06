2e199e6664b8d55cfb5289047c60a6ec599d9309
S-tatsuya <s-tatsuya0123@gmail.com>
2026-08-07 01:34:57 +0900

    chore: Nix の読み込み時間により発生するタイムアウトに対応

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -3,5 +3,10 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      # /nix が別ボリュームにあるためコールドスタート時に git の起動が
+      # デフォルトの 500ms を超えることがある
+      command_timeout = 1000;
+    };
   };
 }
